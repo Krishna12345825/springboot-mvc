3d54150fcf4960abaa571a7b023c4e87e04a7600
KRISHNA KUMAR <krishnabhai117@gmail.com>
2022-11-13 15:43:20 +0530

    server

---
+++ BootMVCProj22-Servers/src/main/java/com/nt/BootMvcProj22ServersApplication.java
@@ -0,0 +1,14 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj22ServersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj22ServersApplication.class, args);
+		System.out.println("Krishna");
+	}
+
+}
